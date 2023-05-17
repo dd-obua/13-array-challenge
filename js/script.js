02c7f4e6a325f7099478f2ef72592bd3b8787e2f
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-17 14:08:14 +0300

    Call checkDogs function using the second test data

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 const dogsJulia = [3, 5, 2, 12, 7];
 const dogsKate = [4, 1, 15, 8, 3];
 
+const dogsJulia2 = [9, 16, 6, 8, 3];
+const dogsKate2 = [10, 5, 6, 1, 4];
+
 const checkDogs = function (dogs1, dogs2) {
   const juliasArr = dogs1.slice(1, -2);
   const dogs = [...juliasArr, ...dogs2];
@@ -17,3 +20,5 @@ const checkDogs = function (dogs1, dogs2) {
 };
 
 checkDogs(dogsJulia, dogsKate);
+console.log();
+checkDogs(dogsJulia2, dogsKate2);
