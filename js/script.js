cbcbc605cbdc4ba6f5975f806598470fee89310c
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-17 14:06:25 +0300

    Log adult dogs and puppies to the console

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ const dogsKate = [4, 1, 15, 8, 3];
 const checkDogs = function (dogs1, dogs2) {
   const juliasArr = dogs1.slice(1, -2);
   const dogs = [...juliasArr, ...dogs2];
+
+  dogs.forEach(function (dog, i) {
+    console.log(
+      `Dog number ${i + 1} is ${
+        dog < 3 ? 'still a puppy 🐶' : `an adult, and is ${dog} years old.`
+      }`
+    );
+  });
 };
 
 checkDogs(dogsJulia, dogsKate);
