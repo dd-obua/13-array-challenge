1968c5127f27bfa464ea53d738bca029674a29c7
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-17 13:31:54 +0300

    Combine Julia's and Kate's data into one

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const dogsKate = [4, 1, 15, 8, 3];
 
 const checkDogs = function (dogs1, dogs2) {
   const juliasArr = dogs1.slice(1, -2);
+  const dogs = [...juliasArr, ...dogs2];
 };
 
 checkDogs(dogsJulia, dogsKate);
