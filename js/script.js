d57cf26ed42c2c61e13e6e23c307398b6c787fde
Obua Denis Daniel <dd.obua@gmail.com>
2023-05-17 13:29:19 +0300

    Remove cats' data from julia's array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+const checkDogs = function (dogs1, dogs2) {
+  const juliasArr = dogs1.slice(1, -2);
+};
+
+checkDogs(dogsJulia, dogsKate);
